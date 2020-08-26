8e734f5948082c7ddd821ce77069abcb24f8252c
Łukasz Królak <lukasz.krolak@gmail.com>
2020-08-26 22:53:45 +0200

    react start

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './Hero.scss';
+
+
+class Hero extends React.Component {
+  render() {
+    return (
+      <header className={styles.component}>
+        <img className={styles.image} src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png"/>
+        <h2 className={styles.title}>Things to do</h2>
+      </header>
+    )
+  }
+}
+
+export default Hero;
