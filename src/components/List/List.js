c01ecfb472394ae335d9164e1bbb8e10bfbb178a
Łukasz Królak <lukasz.krolak@gmail.com>
2020-09-06 19:03:25 +0200

    add colums

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,55 +3,28 @@ import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
 import PropTypes from 'prop-types';
 import Column from '../Column/Column.js';
-import {settings} from '../../data/dataStore';
-import Creator from '../Creator/Creator.js';
-
-
 class List extends React.Component {
-  state = {
-    columns: this.props.columns || [],
-  }
   static propTypes = {
     title: PropTypes.array.isRequired,
-    description: PropTypes.node,
-    columns: PropTypes.array,
+    children: PropTypes.node.isRequired,
   }
   static defaultProps = {
-    description: settings.defaultListDescription,
-  }
-  addColumn(title){
-    this.setState(state => (
-      {
-        columns: [
-          ...state.columns,
-          {
-            key: state.columns.length ? state.columns[state.columns.length-1].key+1 : 0,
-            title,
-            icon: 'list-alt',
-            cards: []
-          }
-        ]
-      }
-    ));
+    children: <p>I can do all the things!!!</p>,
   }
   render() {
     return (
       <section className={styles.component}>
-        <Hero titleText={this.props.title} imageHero={this.props.image} />
+        <Hero titleText={this.props.title} imageHero="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png" />
         <div className={styles.description}>
-          {this.props.description}
+          {this.props.children}
         </div>
         <div className={styles.columns}>
-          {this.state.columns.map(({key, ...columnProps}) => (
-          <Column key={key} {...columnProps} />
-          ))}
-        </div>
-        <div className={styles.creator}>
-          <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)}/>
+          <Column titleColumn={'Animals'}/>
+          <Column titleColumn={'Planets'}/>
+          <Column titleColumn={'Minerals'}/>
         </div>
       </section>
     )
   }
 }
-
 export default List;
